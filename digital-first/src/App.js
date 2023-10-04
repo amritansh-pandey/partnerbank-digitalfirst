bfb5e51152f5bfde44cb15a38f1f1377aab30f70
amritansh-pandey <amritansh.p1010@gmail.com>
2023-10-04 07:25:17 +0530

    acquire

---
+++ digital-first/src/App.js
@@ -0,0 +1,52 @@
+import "./global.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div>
+          <span className="headline1">Headline 1</span>
+        </div>
+        <div>
+          <span className="headline2">Headline 2</span>
+          <div>
+            <span className="heading">Heading</span>
+          </div>
+          <div>
+            <span className="title1">Title 1</span>
+          </div>
+          <div>
+            <span className="title2">Titel 2</span>
+          </div>
+          <div>
+            <span className="subheading1">Sub Heading 1</span>
+          </div>
+          <div>
+            <span className="subheading2">Sub Heading 2</span>
+          </div>
+          <div>
+            <span className="body1">Body 1</span>
+          </div>
+          <div>
+            <span className="body2">Body 2</span>
+          </div>
+          <div>
+            <span className="caption">Caption</span>
+          </div>
+          <div>
+            <span className="regular-narrow">Regular Narrow</span>
+          </div>
+          <div>
+            <span className="medium-narrow">Medium Narrow</span>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="color pallete primary"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
